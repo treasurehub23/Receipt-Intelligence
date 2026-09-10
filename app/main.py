c686fd88243284451700cefd29ea31abc92b55d4
treasurehub23 <okegbolatreasure@gmail.com>
2026-09-10 13:35:41 +0100

    first

---
+++ app/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, Depends, File, UploadFile, HTTPException
+from pydantic import BaseModel
+import cv2 as cv
+import numpy as np
+app = FastAPI()
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
+
+class Expense(BaseModel):
+    id: int
+    amount: float
+    description: str
+
+@app.post("/upload")
+async def upload_expense(file: UploadFile = File(...)):
+    accepted_types = ["image/jpeg", "image/png"]
+
+    if file.content_type not in accepted_types:
+        raise HTTPException(status_code=400, detail="Invalid file type. Only JPEG and PNG are accepted.")
+
+
+    content = await file.read()  # Read the file content (you can process it as needed)
+
+    max_Size = 5 * 1024 * 1024  
+    if len(content) > max_Size:
+        raise HTTPException(status_code=400, detail="File size exceeds the maximum limit of 5MB.")
+    nparray = np.frombuffer(content, np.uint8)
+    image = cv.imdecode(nparray, cv.IMREAD_COLOR)
+    cv.imshow("Uploaded Image", image)
+    cv.imwrite("screenshot.png", image)
+    return {"id": 1, "amount": 100.0, "description": "Sample expense uploaded successfully."}
+
+cap = cv.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to capture image from webcam.")
+        break
+    video = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow("Webcam Image", video)
+    if cv.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv.destroyAllWindows()
